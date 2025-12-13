6f01138d98874610023381cf4e525f128b62fb86
min <min@email.com>
2025-12-13 06:21:25 -0800

    timer work

--- applications/blinky/src/main_th.c
+++ applications/blinky/src/main_th.c
@@ -13,28 +13,48 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 #define MY_DEV_IRQ              24
 #define MY_DEV_PRIO             2
 
+/// @brief semaphore  
+K_SEM_DEFINE(led_sem, 0, 1); //name, init-count, limit)
 
 
-K_SEM_DEFINE(sem_led, 0, 1); //name, init-count, limit)
+/// @brief working thread handler
+/// @param work 
+void work_handler(struct k_work *work)
+{
+    // do something
+}
+K_WORK_DEFINE(mywork, work_handler);
 
+/// @brief timer ISR, call work thread
+/// @param timer_id 
+void timer_callback(struct k_timer *timer_id)
+{
+    // dump to working thread
+    k_work_submit(&mywork);
+}
 
-void thread_entry_led(void *arg1, void *arg2, void *arg3)
+void led_task(void *arg1, void *arg2, void *arg3)
 {
     while(1) {
 
-        k_sem_take(&sem_led, K_FOREVER); //Wait
+        k_sem_take(&led_sem, K_FOREVER);
         print("do something...\n");
-        k_sem_give(&my_sem); // Release semaphore        
+        k_sem_give(&led_sem); // Release semaphore        
 
         k_msleep(TIME_1000MS);
     }
 }
 
+/// @brief 
+/// @param  
+/// @return 
 int main(void)
 {
-    K_THREAD_DEFINE(tid_sw0,
+    K_TIMER_DEFINE(my_timer, timer_callback, NULL);
+    
+    K_THREAD_DEFINE(sw0_tid,
                     MY_STACK_SIZE,
-                    thread_entry_led, NULL, NULL, NULL,
+                    led_task, NULL, NULL, NULL,
                     MY_HIGH_PRIORITY,
                     0,  /* option */
                     0); /* no delay */
@@ -42,7 +62,7 @@ int main(void)
     print("Hello blinky\n");
     while(1) {
                 
-        k_sem_give(&sem_led); //Signal to 
+        k_sem_give(&led_sem); // Signal to 
 
         k_msleep(TIME_1000MS);
     }
